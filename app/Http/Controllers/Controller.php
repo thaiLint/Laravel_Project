a075bbc8e95fc241e1c3738203964a0736ee13e5
unknown <tham.chanthy.1223@rupp.edu.kh>
2026-05-14 09:48:08 +0700

    calendar ui

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,8 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Route;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
 }
+
+//Route::get('/', [HomeController::class, 'index']);
+Route::get('/dashboard', [DashboardController::class, 'index']);
+Route::get('/calendar', [CalendarController::class, 'index']);
+Route::get('/customer', [CustomerController::class, 'index']);
+//Route::get('/room', [RoomController::class, 'index']);
