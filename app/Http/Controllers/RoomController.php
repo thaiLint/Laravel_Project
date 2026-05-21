71046a035d602ac94ee48ba452bcf4bcefdc2075
Thai Lineth <thailinet08@gmail.com>
2026-05-21 19:41:02 +0700

    pull from chhenghorng

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -16,17 +16,13 @@ class RoomController extends Controller
         return view('rooms.index', compact('rooms'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+    
     public function create()
     {
         return view('rooms.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+    
     public function store(Request $request)
     {
         $request->validate([
@@ -41,25 +37,19 @@ class RoomController extends Controller
             ->with('success', 'Room created successfully.');
     }
 
-    /**
-     * Display the specified resource.
-     */
+  
     public function show(Room $room)
     {
         return view('rooms.show', compact('room'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
+   
     public function edit(Room $room)
     {
         return view('rooms.edit', compact('room'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+  
     public function update(Request $request, Room $room)
     {
         $request->validate([
@@ -74,9 +64,7 @@ class RoomController extends Controller
             ->with('success', 'Room updated successfully.');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+  
     public function destroy(Room $room)
     {
         $room->delete();
